da4c4ee168fb3117f902db204f5e65a990d15615
charlieTai <charlie.tai@tpisoftware.com>
2026-02-08 16:13:33 +0800

    add : 實現調整

--- src/modules/analytics/analytics.module.ts
+++ src/modules/analytics/analytics.module.ts
@@ -1,12 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
 import { AnalyticEvent } from './entities/analytics.entity';
 import { AnalyticsInterceptor } from './interceptors/analytics.interceptor';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AnalyticEvent])],
+  imports: [
+    TypeOrmModule.forFeature([AnalyticEvent]),
+    forwardRef(() => ProductsModule), // 使用 forwardRef 處理循環依賴
+  ],
   controllers: [AnalyticsController],
   providers: [AnalyticsService, AnalyticsInterceptor],
   exports: [AnalyticsService, AnalyticsInterceptor], // 匯出以便其他模組使用
--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -9,9 +9,10 @@ import { AnalyticsModule } from '../analytics/analytics.module';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Product, ProductCategory]),
-    AnalyticsModule, // 💡 導入 AnalyticsModule 以使用 AnalyticsInterceptor
+    forwardRef(() => AnalyticsModule), // 💡 使用 forwardRef 處理循環依賴
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
+  exports: [ProductsService], // 導出 ProductsService 供其他模組使用
 })
 export class ProductsModule {}
